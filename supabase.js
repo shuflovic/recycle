d7e7c3a825c00c0c21e1fc5149ab1e35e68c78e9
shuflovic <pavel.skuta@gmail.com>
2025-10-10 20:26:51 +1300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -3,6 +3,8 @@ require('dotenv').config(); // load .env variables
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_KEY = process.env.SUPABASE_KEY;
 
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+const { createClient } = require('@supabase/supabase-js');
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // export { supabaseClient };
